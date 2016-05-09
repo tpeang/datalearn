9bb013f5cc4abed58aa3916176bde80d6479214f
tpeang <tpeang@outlook.com>
2016-05-09 14:08:12 +0800

    testTN

--- src/dataLearn/Algorithm51.java
+++ src/dataLearn/Algorithm51.java
@@ -211,6 +211,28 @@ public class Algorithm51 {
 		System.out.println("sum=" + sum);
 	}
 
+	public static void testTN() {
+		/**
+		 * 题目：有1、2、3、4个数字，能组成多少个互不相同且无重复数字的三位数？都是多少？
+		 * 1.程序分析：可填在百位、十位、个位的数字都是1、2、3、4。 组成所有的排列后再去 掉不满足条件的排列。
+		 *
+		 */
+
+		int a = 0, b = 0, c = 0;
+		int count = 1;
+		for (a = 1; a < 5; a++) {
+			for (b = 1; b < 5; b++) {
+				for (c = 1; c < 5; c++) {
+					if (a != b && a != c && b != c) {
+						System.out.println("NO." + count + ":" + a + " " + b
+								+ " " + c);
+						count++;
+					}
+				}
+			}
+		}
+	}
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// rabbit();
@@ -219,8 +241,9 @@ public class Algorithm51 {
 		// resolveNum();
 		// strIdentify();
 		// numAdd();
-		 wanShu();
-		testBall();
+		// wanShu();
+		// testBall();
+		testTN();
 	}
 
 }
